295a2a56147fe3f36d40f72de0212f8efc6fba79
QSP <QSP@QSP>
2021-07-27 19:37:16 +0530

    dummy

---
+++ src/test/java/authorizationGitHub/DeleteRepro2.java
@@ -0,0 +1,15 @@
+package authorizationGitHub;
+import static io.restassured.RestAssured.*;
+import org.testng.annotations.Test;
+
+public class DeleteRepro2 {
+	@Test
+	public void tc4() {
+		given()
+        .auth().oauth2("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")
+		.delete("https://api.github.com/repos/javaandselenium/wwwwwww09")
+		.then().log().all();
+		
+	}
+
+}
